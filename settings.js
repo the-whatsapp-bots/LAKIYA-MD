03da2eda78874fac0b8df1b083913cdcc326b317
the-whatsapp-bots <cartaxjobs@gmail.com>
2025-06-08 20:04:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 //================
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Lakiya-Md=eEFlCDZY#-_tfyfJLs6irmtx7wZeFCvyUAqWvbc1EFQ8pDg3LWhw' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Lakiya-Md=vYUQBZiY#kvQJlb7ipi0-2aJ5ikyQsWiTGONgEMR-Tj0Hki1zGbk' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"privet" : process.env.MODE,
